41673a3406104249cf41680b98839d1c84d71633
Pepe Melendez <pepe_ciro@hotmail.com>
2018-04-16 14:21:27 -0500

    Initial structure
    Define initial structs for plc inputs, outputs, and whole.

--- arduino/plcmonitor/plcmonitor.ino
+++ arduino/plcmonitor/plcmonitor.ino
@@ -1,13 +1,156 @@
-//Define plc struct
+#define ANALOG_INPUT_COUNT 6
+#define DIGITAL_INPUT_COUNT 6
+#define INPUT_COUNT (ANALOG_INPUT_COUNT + DIGITAL_INPUT_COUNT)
+#define OUTPUT_COUNT 6
+
+/** Clear object using pointer */
+#define clearObject(p) memset((p), 0, sizeof(*(p)))
+
+/* Digital input types */
+enum
+{
+  type_Digital = 0, ///< Digital input
+  type_Counter, ///< Incremental counter input
+  type_Analog, ///< Analog input
+}di_types_e;
+
+/* Digital output actions */
+enum
+{
+  action_None = 0, ///< No action
+  action_Permanent, ///< Remain turned on
+  action_Event, ///< Turn on while input above threshold
+  action_Delay, ///< Turn on certain amount of time
+}do_actions_e;
+
+/* Input struct */
+typedef struct plc_in_t plc_in_t;
+struct plc_in_t
+{
+  String nametag; ///< Nametag identifier
+  uint8_t type; ///< Input type
+  uint32_t value; ///< Input value
+};
+
+/* Digital output struct */
+typedef struct plc_do_t plc_do_t;
+struct plc_do_t
+{
+  String nametag; ///< Nametag identifier
+  uint8_t value; ///< Digital output value
+  uint8_t actions[INPUT_COUNT]; ///< Output action
+};
+
+/* Plc struct */
+typedef struct PlcDevice PlcDevice;
+struct PlcDevice
+{
+  plc_in_t in[INPUT_COUNT];
+  plc_in_t * din;
+  plc_in_t * ain;
+  plc_do_t dout[OUTPUT_COUNT];
+  String nametag;
+  uint8_t ip[4];
+};
+
+/* Initialize plcDevice */
+void _plcDeviceInit(PlcDevice *d)
+{
+  uint8_t i = 0;
+  String n;
+  
+  clearObject(d);
+  d->nametag = "PLC";
+  
+  d->din = d->in;
+  d->ain = &d->in[6];
+
+  // Initialize inputs and outputs
+  for (i = 0; i < DIGITAL_INPUT_COUNT; i ++)
+  {
+    d->din[i].type = type_Digital;
+    d->din[i].nametag = "DI" + String(i);
+
+    d->ain[i].type = type_Analog;
+    d->ain[i].nametag = "AI" + String(i);
+
+    d->dout[i].nametag = "DO" + String(i);
+  }  
+}
+
+/* Digital output actions string */
+String _actionsString(plc_do_t * d)
+{
+  String n = "";
+  uint8_t i;
+  for (i = 0; i < OUTPUT_COUNT; i ++)
+  {
+    n = n + String(d->actions[i]) + " ";
+  }
+  return n;
+}
+
+/* Digital input types string name */
+String _typeString(uint8_t n)
+{
+  switch(n)
+  {
+    case type_Digital: return "Digital"; break;
+    case type_Counter: return "Counter"; break;
+    case type_Analog: return "Analog"; break;
+    default: return "None"; break;
+  }
+  return "None";
+}
+
+/* Ip string */
+String _ipString(uint8_t * ip)
+{
+  String s = "";
+  uint8_t i;
+  for (i = 0; i < 3; i ++)
+    s = s + String(ip[i]) + ".";
+  s = s + String(ip[3]);
+  return s;
+}
+
+/* Print device */
+void _printPlcDevice(PlcDevice *d)
+{
+  Serial.println("Name: " + String(d->nametag));
+  Serial.println("IP: " + _ipString(d->ip));
+
+  uint8_t i;
+  for (i = 0; i < INPUT_COUNT; i ++)
+  {
+    Serial.print("Input #" + String(i));
+    Serial.print("\tName: " + d->in[i].nametag);
+    Serial.print("\tType: " + _typeString(d->in[i].type));
+    Serial.println("\tValue: " + String(d->in[i].value)); 
+  }
+
+  for(i = 0; i < OUTPUT_COUNT; i ++)
+  {
+    Serial.print("Output #" + String(i));
+    Serial.print("\tName: " + d->dout[i].nametag);
+    Serial.print("\tValue: " + String(d->dout[i].value));
+    Serial.println("\tActions: " + _actionsString(&d->dout[i]));
+  }
+}
+
+/* Global variables */
+PlcDevice plcDevice;
 
 void setup() 
 {
-  // put your setup code here, to run once:
-
+  Serial.begin(9600);
+  delay(100);
+  
+  _plcDeviceInit(&plcDevice);
+  _printPlcDevice(&plcDevice);
 }
 
 void loop() 
 {
-  // put your main code here, to run repeatedly:
 
 }
